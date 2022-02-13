97827a2acb58fe0204a8cfc0bf9d20a7a97a87a9
rojinald <rodjinals@gmail.com>
2022-02-13 11:34:33 +0300

    upd

--- src/components/Sidebar/SubMenu/SubMenuLast/SubMenuLast.js
+++ src/components/Sidebar/SubMenu/SubMenuLast/SubMenuLast.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import ContextMenu from '../../../Context/ContextMenu'
-import UserRightClickMenu from '../../../Context/userRightClickMenu/userRightClickMenu'
 import s from './SubMenuLast.module.css'
 
 
@@ -9,12 +8,30 @@ import s from './SubMenuLast.module.css'
 const SubMenuLast = ({ item }) => {
    const [subnav, setSubnav] = useState(false)
    const showSubnav = () => setSubnav(!subnav)
-   const {x,y,showMenu} = UserRightClickMenu()
+ 
+   const [x, setX] = useState(0)
+   const [y, setY] = useState(0)
+   const [showMenu, setShowMenu] = useState(false)
+
+   const handleContextMenu = (e) => {
+      e.preventDefault()
+      e.pageX + 150 > window.innerWidth ? setX(`${window.innerWidth - 180}px`) : setX(e.pageX)
+      e.pageY + 200 > window.innerHeight ? setY(`${window.innerHeight - 230}px`) : setY(e.pageY)
+      setShowMenu(true)
+   
+   }
+
+   const handleClick = () => {
+      showMenu && setShowMenu(false)
+   }
+
+
+
 
 
    return  <div className={s.sub_menu_items}>
          <ContextMenu x={x} y={y} showMenu={showMenu} />
-         <Link to={item.path} onClick={item.subNavLast && showSubnav}>
+         <Link to={item.path} onClick={item.subNavLast && showSubnav} onContextMenu={handleContextMenu} onClick={handleClick}>
             <div className={s.sub_menu_item}>
                <div className={s.sub_menu_item_icon}>
                   {item.subNavLast && subnav
